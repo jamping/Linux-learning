7d09a8fffa8a89f276142a4328aec1bd24f8bc1c
jjp123 <jjp123@aliyun.com>
2017-11-09 08:35:40 +0800

    eth_bond modified

--- eth_bond.sh
+++ eth_bond.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-#Auto Make KVM Virtualization
+#created by jjp 2017.11.09
 #Auto config bond scripts
 function eth_bond()
 {
 	NETWORK=(
-		HWADDR=`ifconfig eth0 |egrep "HWaddr|Bcast" |tr "\n" " "|awk '{print $5,$7,$NF}'|sed -e 's/addr://g' -e 's/Mask://g'|awk '{print $1}'`
-		IPADDR=`ifconfig eth0 |egrep "HWaddr|Bcast" |tr "\n" " "|awk '{print $5,$7,$NF}'|sed -e 's/addr://g' -e 's/Mask://g'|awk '{print $2}'`
-		NETMASK=`ifconfig eth0 |egrep "HWaddr|Bcast" |tr "\n" " "|awk '{print $5,$7,$NF}'|sed -e 's/addr://g' -e 's/Mask://g'|awk '{print $3}'`
+		HWADDR=`ifconfig $1 |egrep "HWaddr|Bcast" |tr "\n" " "|awk '{print $5,$7,$NF}'|sed -e 's/addr://g' -e 's/Mask://g'|awk '{print $1}'`
+		IPADDR=`ifconfig $1 |egrep "HWaddr|Bcast" |tr "\n" " "|awk '{print $5,$7,$NF}'|sed -e 's/addr://g' -e 's/Mask://g'|awk '{print $2}'`
+		NETMASK=`ifconfig $1 |egrep "HWaddr|Bcast" |tr "\n" " "|awk '{print $5,$7,$NF}'|sed -e 's/addr://g' -e 's/Mask://g'|awk '{print $3}'`
 		GATEWAY=`route -n|grep "UG"|awk '{print $2}'`
 	)
 cat >ifcfg-$1<<EOF
