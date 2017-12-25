44758c6d41c970cc70e689bf721b4981677eab65
jjp123 <jjp123@aliyun.com>
2017-12-25 20:21:01 +0800

    sphinx

--- mirrors/epel.sh
+++ mirrors/epel.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
 #epel
-[ -d /data/www/epel/6/ ] || mkdir -p /data/www/epel/6/
-rsync -avz --exclude-from=/data/www/exclude.list rsync://mirrors.tuna.tsinghua.edu.cn/epel/6/ /data/www/epel/6/
-createrepo --update /data/www/epel/6/
-
-[ -d /data/www/epel/7/ ] || mkdir -p /data/www/epel/7/
-rsync -avz --exclude-from=/data/www/exclude.list rsync://mirrors.tuna.tsinghua.edu.cn/epel/7/ /data/www/epel/7/
-createrepo --update /data/www/epel/7/
+[ -d /data/www/epel/ ] || mkdir -p /data/www/epel/
+rsync -avz --exclude-from=/data/www/exclude.list rsync://mirrors.tuna.tsinghua.edu.cn/epel//data/www/epel/
+createrepo  /data/www/epel/
